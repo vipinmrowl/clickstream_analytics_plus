8ee12bb2a00ebcae91b0ea8d7c524674b0d26568
Vipin Kashyap <vipin@drowl.com>
2025-10-29 14:18:23 -0700

    Working android implementation

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -32,11 +33,20 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+            )
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
---
+++ example/android/app/proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class software.aws.solution.clickstream.** { *; }
